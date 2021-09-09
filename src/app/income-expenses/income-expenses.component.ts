c7dca41f592313699a42dda0e4213fadacbd0fe6
Lucas Peixoto <lspeixoto@stefanini.com>
2021-09-09 09:20:25 -0300

    Inclusão da estrutura de componentes

---
+++ src/app/income-expenses/income-expenses.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-income-expenses',
+  templateUrl: './income-expenses.component.html',
+  styleUrls: ['./income-expenses.component.scss']
+})
+export class IncomeExpensesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
