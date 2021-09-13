03c3b92440b07e724bd8c414faf1bc530c223ead
Lucas Peixoto <lspeixoto@stefanini.com>
2021-09-12 23:17:24 -0300

    Inclusão da função initiIncomeExpenseListener para obter a coleção de items para o usuário ativo

--- src/app/incomes-expenses/components/dashboard/dashboard.component.ts
+++ src/app/incomes-expenses/components/dashboard/dashboard.component.ts
@@ -1,12 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { filter, take } from 'rxjs/operators';
+import { User } from 'src/app/auth/models/user.model';
+import * as fromRoot from '../../../app.reducer';
+import { IncomeExpenseService } from '../../services/income-expense.service';
 @Component({
 	selector: 'app-dashboard',
 	templateUrl: './dashboard.component.html',
 	styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
-	constructor() {}
+	user$: Observable<User>;
+	constructor(
+		private store: Store<fromRoot.AppState>,
+		private incomeExpenseService: IncomeExpenseService,
+	) {}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		this.store
+			.select(fromRoot.getIsAuth)
+			.pipe(
+				filter(user => user !== null),
+				take(1),
+			)
+			.subscribe((user: User) => {
+				this.incomeExpenseService.initIncomeExpenseListener(user.userId)
+        .subscribe(items => console.log(items))
+			});
+	}
 }
--- src/app/incomes-expenses/services/income-expense.service.ts
+++ src/app/incomes-expenses/services/income-expense.service.ts
@@ -1,31 +1,26 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
-import {
-	AngularFirestore,
-	AngularFirestoreDocument,
-} from '@angular/fire/firestore';
-import firebase from 'firebase/app';
-import { Router } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 //* NgRx
 import { Store } from '@ngrx/store';
 
 //* Redux
 import * as UI from '../../shared/store/ui/ui.actions';
-import * as AUTH from '../../auth/store/auth.actions';
 import * as fromRoot from '../../app.reducer';
 import { IncomeExpense } from '../models/income-expense.model';
 import { AuthService } from 'src/app/auth/services/auth.service';
 
 //* Mensagens
 import Swal from 'sweetalert2';
-import { Title, Text } from '../../shared/messages/errors';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { map, take } from 'rxjs/operators';
 
 @Injectable({
 	providedIn: 'root',
 })
 export class IncomeExpenseService {
 	constructor(
+		private angularFireAuth: AngularFireAuth,
 		private angularFirestore: AngularFirestore,
 		private store: Store<fromRoot.AppState>,
 		private authService: AuthService,
@@ -35,7 +30,7 @@ export class IncomeExpenseService {
 		this.store.dispatch(UI.StartLoading());
 		const userId = this.authService.user.userId;
 		this.angularFirestore
-			.collection(`users/${userId}/incomes-expenses`)
+			.collection(`users/${userId}/items`)
 			.add({ ...item })
 			.then(() => {
 				this.store.dispatch(UI.StopLoading());
@@ -54,5 +49,19 @@ export class IncomeExpenseService {
 					text: `Tente novamente, caso o erro pesista contate o administrador`,
 				});
 			});
+	}
+
+	initIncomeExpenseListener(userId: string) {
+		return this.angularFirestore
+			.collection(`users/${userId}/items`)
+			.snapshotChanges()
+			.pipe(
+				map(snapshot =>
+					snapshot.map(doc => ({
+						id: doc.payload.doc.id,
+						...(doc.payload.doc.data() as IncomeExpense),
+					})),
+				),
+			);
 	}
 }
