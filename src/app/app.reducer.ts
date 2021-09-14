24f534a803303ef04b159a8a3e68fe7c09cdf77e
Lucas Peixoto <lspeixoto@stefanini.com>
2021-09-14 09:57:20 -0300

    Separação dos selectors em arquivos _específicos

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -1,8 +1,4 @@
-import {
-	ActionReducerMap,
-	createFeatureSelector,
-	createSelector,
-} from '@ngrx/store';
+import { ActionReducerMap } from '@ngrx/store';
 
 import * as fromUi from './shared/store/ui.reducer';
 import * as fromAuth from './auth/store/auth.reducer';
@@ -16,17 +12,3 @@ export const reducers: ActionReducerMap<AppState> = {
 	ui: fromUi.uiReducer,
 	auth: fromAuth.authReducer,
 };
-
-//* Selectors
-/* export const getUiState = createFeatureSelector<fromUi.UiState>('ui');
-export const getIsLoading = createSelector(getUiState, fromUi.getIsLoading); */
-
-/* export const getAuthState = createFeatureSelector<fromAuth.AuthState>('auth');
-export const getIsAuth = createSelector(getAuthState, fromAuth.getUserData);
- */
-/* export const getIncomeExpenseState =
-	createFeatureSelector<fromIncomeExpense.IncomeExpenseState>('incomeExpense');
-export const getIncomeExpensesData = createSelector(
-	getIncomeExpenseState,
-	fromIncomeExpense.getIncomeExpenseData,
-); */
