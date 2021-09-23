1e4dadf4a997b4df80b0481ab7b08c04ec72ebe1
Lucas Peixoto <lspeixoto@stefanini.com>
2021-09-23 09:07:58 -0300

    Inclusão da opção de frequência da despesa

--- src/app/incomes-expenses/components/incomes-expenses/incomes-expenses.component.ts
+++ src/app/incomes-expenses/components/incomes-expenses/incomes-expenses.component.ts
@@ -13,6 +13,25 @@ export class IncomeExpensesComponent implements OnInit {
 	incomeExpenseForm: FormGroup;
 	type: Item = 'income';
 
+	categoryOptions: string[] = [
+		'Moradia',
+		'Alimentação',
+		'Saúde',
+		'Educação',
+		'Despesas Pessoais',
+		'Transporte',
+		'Celular/TV/Internet',
+		'Casa',
+		'Carro',
+		'Emergência',
+		'Lazer',
+	];
+
+	frequencyOptions: string[] = [
+    'Fixa',
+    'Variável'
+  ];
+
 	constructor(
 		private formBuilder: FormBuilder,
 		private incomeExpenseService: IncomeExpenseService,
@@ -22,12 +41,23 @@ export class IncomeExpensesComponent implements OnInit {
 		this.incomeExpenseForm = this.formBuilder.group({
 			description: ['', Validators.required],
 			amount: ['', Validators.required],
+			category: ['', Validators.required],
+			frequency: ['', Validators.required],
+			date: [new Date(), Validators.required],
 		});
 	}
 
 	onSubmit() {
-		const { description, amount } = this.incomeExpenseForm.value;
-		const incomeExpense = new IncomeExpense(description, amount, this.type);
+		const { description, amount, category, frequency, date } =
+			this.incomeExpenseForm.value;
+		const incomeExpense = new IncomeExpense(
+			description,
+			amount,
+			category,
+			frequency,
+			date,
+			this.type,
+		);
 		this.incomeExpenseService.createIncomeOrExpense(incomeExpense);
 	}
 }
--- src/app/incomes-expenses/models/income-expense.model.ts
+++ src/app/incomes-expenses/models/income-expense.model.ts
@@ -2,6 +2,9 @@ export class IncomeExpense {
 	constructor(
 		public description: string,
 		public amount: number,
+    public category: string,
+    public frequency: string,
+    public date: string,
 		public type: string,
 	) {}
 }
